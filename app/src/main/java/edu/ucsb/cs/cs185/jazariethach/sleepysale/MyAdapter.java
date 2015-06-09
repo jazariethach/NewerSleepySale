689dcfcefde694742f30b97fed2329ed0d716b85
Felicia <xftruong@hotmail.com>
2015-06-08 20:09:00 -0700

    Intents

--- app/src/main/java/edu/ucsb/cs/cs185/jazariethach/sleepysale/MyAdapter.java
+++ app/src/main/java/edu/ucsb/cs/cs185/jazariethach/sleepysale/MyAdapter.java
@@ -3,13 +3,22 @@ package edu.ucsb.cs.cs185.jazariethach.sleepysale;
 /**
  * Created by Jazarie on 6/5/2015.
  */
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.squareup.picasso.Picasso;
 import java.util.List;
 
@@ -81,6 +90,20 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.CardViewHolder> {
             type = (TextView) v.findViewById(R.id.buyType);
             tags = (TextView) v.findViewById(R.id.tags);
             iv = (ImageView) v.findViewById(R.id.imageView);
+            Context context = iv.getContext();
+            final Intent nextScreen = new Intent(context, ItemActivity.class);
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = iv.getContext();
+                    String s = vTitle.getText().toString() + "%" + vPrice.getText().toString() + "%" + type.getText().toString()
+                            + "%" + vDesc.getText().toString() + "%" + date.getText().toString() + "%";
+                    nextScreen.putExtra("test", s);
+                    context.startActivity(nextScreen);
+//                    Toast.makeText(context, s, Toast.LENGTH_LONG).show();
+                }
+            });
+
         }
     }
 }
